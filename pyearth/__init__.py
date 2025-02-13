e8d562d3a4a150ba6e6ae90e618c5996a6791864
Felix Williams <felixgwilliams@users.noreply.github.com>
2025-02-13 20:18:59 +0000

    remove versioneer and setup hatch

--- pyearth/__init__.py
+++ pyearth/__init__.py
@@ -4,7 +4,6 @@ Created on Feb 16, 2013
 @author: jasonrudy
 """
 
-from . import _version
 from .earth import Earth as Earth
 
-__version__ = _version.get_versions()["version"]
+__version__ = "0.2.0"
